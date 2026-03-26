1ee0b9f16340143487d248768551744f13669529
angelxim13 <angelpedroza1206@gmail.com>
2026-03-25 21:28:53 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,13 @@ function Navbar({ irInicio }) {
   return (
     <div className="navbar">
 
-      <h2 className="logo">VisionScan 🔎</h2>
+      <h2 className="logo" onClick={irInicio}>
+        VisionScan 🔎
+      </h2>
 
       <div className="nav-buttons">
         <button onClick={irInicio}>Inicio</button>
+        <button>Información</button>
       </div>
 
     </div>
